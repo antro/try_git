da78a5683cdbf96c49856f71ce9c19c46115e845
Antro <halaczkiewicz@gmail.com>
2012-12-23 14:35:36 +0100

    new project blog

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '73842d6dc6615eec88ce6541d0805637a4a829f37b857111d11ffebd7696fd133d2b1a75c1aeae7883905a0b95d93b535dff9d6ecffb116918c83dc1dc75e6f4'
